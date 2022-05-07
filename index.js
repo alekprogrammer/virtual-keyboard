8692d5abc8020c8cb06bf7fb7e97cb6716dbfabb
Aleksandr Lobkov <das149848@gmail.com>
2022-05-07 16:52:41 +0400

    feat: add 2,3,4 rows of keys in keyboard

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
+const text = "Virtual Keyboard by Aleksandr Lobkov";
+const title = document.createElement("h1");
+let spans = [];
+for (let i = 0; i < text.length; i++) {
+    spans.push(document.createElement("span"))
+    spans[i].innerHTML = text[i]
+    title.append(spans[i]);
+}
+let delay = 0;
+for (let i = 0; i < spans.length; i++) {
+    spans[i].style.animationDelay = delay + "s";
+    delay = delay + 0.2;
+}
+document.body.append(title);
+
 const KEYBOARD = document.createElement("div");
 KEYBOARD.classList.add("keyboard");
 const fKeysContainer = document.createElement("div")
 fKeysContainer.classList.add("upnumbers-keys-container")
 const fKeys = [];
+const backtick = document.createElement("div");
+backtick.classList.add("standart-key");
+backtick.innerHTML = "`"
 const backspace = document.createElement("div");
-//backtick
-backspace.classList.add("backspace")
-backspace.innerHTML = "BACKSPACE"
-for (let i = 0; i < 12; i++) {
+backspace.classList.add("backspace");
+backspace.innerHTML = "BACKSPACE";
+fKeysContainer.append(backtick)
+for (let i = 0; i < 9; i++) {
     fKeys.push(document.createElement("div"));
     fKeys[i].classList.add("standart-key");
-    fKeys[i].innerHTML = i;
+    fKeys[i].innerHTML = i + 1;
     fKeysContainer.append(fKeys[i]);
 }
-
+fKeys.push(document.createElement("div"));
+fKeys[9].classList.add("standart-key");
+fKeys[9].innerHTML = 0;
+fKeysContainer.append(fKeys[9]);
 document.body.append(KEYBOARD);
+fKeysContainer.append(backspace);
 KEYBOARD.append(fKeysContainer);
-KEYBOARD.append(backspace);
 
-let modifychars = ["TAB", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "SHIFT", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
+let modifychars = ["TAB", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
 
 const TAB = document.createElement("div")
 TAB.classList.add("tab-slash");
@@ -45,6 +66,11 @@ KEYBOARD.append(row_2);
 const row_3 = document.createElement("div");
 row_3.classList.add("row");
 
+
+const CAPSLOCK = document.createElement("div");
+CAPSLOCK.classList.add("caps-lock")
+CAPSLOCK.innerHTML = "CAPS<br>LOCK"
+row_3.append(CAPSLOCK);
 for (let i = 12; i < 23; i++) {
     mainPartKeyboard.push(document.createElement("div"));
     mainPartKeyboard[i].classList.add("standart-key");
@@ -52,6 +78,29 @@ for (let i = 12; i < 23; i++) {
     row_3.append(mainPartKeyboard[i]);
 }
 
+const ENTER = document.createElement("div");
+ENTER.classList.add("enter")
+ENTER.innerHTML = "ENTER"
+row_3.append(ENTER);
+
+row_3.append()
+
 KEYBOARD.append(row_3);
 
-console.log(modifychars)
+
+const row_4 = document.createElement("div");
+row_4.classList.add("row");
+const SHIFT = document.createElement("div");
+SHIFT.classList.add("shift");
+SHIFT.innerHTML = "SHIFT";
+row_4.append(SHIFT);
+for (let i = 23; i < modifychars.length - 1; i++) {
+    mainPartKeyboard.push(document.createElement("div"));
+    mainPartKeyboard[i].classList.add("standart-key");
+    mainPartKeyboard[i].innerHTML = modifychars[i + 1];
+    row_4.append(mainPartKeyboard[i]);
+}
+
+KEYBOARD.append(row_4);
+
+console.log(modifychars);

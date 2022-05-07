39179e4a9ad8257e7366735ca9fa8dc95551329d
Aleksandr Lobkov <das149848@gmail.com>
2022-05-08 02:53:35 +0400

    feat: add 5 row of keys and refactor code make short its

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-const text = "Virtual Keyboard by Aleksandr Lobkov";
+const text = "Virtual Keyboard by alekprogrammer";
 const title = document.createElement("h1");
+
+class Methods {
+    static addelemtopage(elem, clas, inn, row) {
+        elem.classList.add(clas);
+        elem.innerHTML = inn;
+        row.append(elem);
+    }
+    static addrowtopage(rowelem) {
+        rowelem.classList.add("row");
+        KEYBOARD.append(rowelem);
+    }
+}
+
+const TEXTINPUT = document.createElement("textarea")
+document.body.append(TEXTINPUT);
+TEXTINPUT.setAttribute("rows", 10)
+TEXTINPUT.setAttribute("cols", 96)
+TEXTINPUT.setAttribute("placeholder", "Write something...")
+
 let spans = [];
 for (let i = 0; i < text.length; i++) {
     spans.push(document.createElement("span"))
-    spans[i].innerHTML = text[i]
+    spans[i].innerHTML = text[i];
     title.append(spans[i]);
 }
 let delay = 0;
@@ -15,92 +34,114 @@ document.body.append(title);
 
 const KEYBOARD = document.createElement("div");
 KEYBOARD.classList.add("keyboard");
-const fKeysContainer = document.createElement("div")
-fKeysContainer.classList.add("upnumbers-keys-container")
+const row_1 = document.createElement("div")
+
+Methods.addrowtopage(row_1)
 const fKeys = [];
 const backtick = document.createElement("div");
-backtick.classList.add("standart-key");
-backtick.innerHTML = "`"
-const backspace = document.createElement("div");
-backspace.classList.add("backspace");
-backspace.innerHTML = "BACKSPACE";
-fKeysContainer.append(backtick)
+Methods.addelemtopage(backtick, "standart-key", "`", row_1)
+console.log(row_1)
+
+
+
 for (let i = 0; i < 9; i++) {
     fKeys.push(document.createElement("div"));
-    fKeys[i].classList.add("standart-key");
-    fKeys[i].innerHTML = i + 1;
-    fKeysContainer.append(fKeys[i]);
+    Methods.addelemtopage(fKeys[i], "standart-key", i + 1, row_1)
 }
+
 fKeys.push(document.createElement("div"));
-fKeys[9].classList.add("standart-key");
-fKeys[9].innerHTML = 0;
-fKeysContainer.append(fKeys[9]);
+
+Methods.addelemtopage(fKeys[9], "standart-key", 0, row_1)
+
+fKeys.push(document.createElement("div"));
+Methods.addelemtopage(fKeys[10], "standart-key", "-", row_1)
+
+fKeys.push(document.createElement("div"));
+Methods.addelemtopage(fKeys[11], "standart-key", "=", row_1)
+
+const backspace = document.createElement("div");
+Methods.addelemtopage(backspace, "backspace", "BACKSPACE", row_1)
+
 document.body.append(KEYBOARD);
-fKeysContainer.append(backspace);
-KEYBOARD.append(fKeysContainer);
 
 let modifychars = ["TAB", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/"];
 
 const TAB = document.createElement("div")
-TAB.classList.add("tab-slash");
-TAB.innerHTML = modifychars[0];
+Methods.addelemtopage(TAB, "tab-slash", modifychars[0], row_1)
 let mainPartKeyboard = [];
 const row_2 = document.createElement("div")
-row_2.classList.add("row")
+Methods.addrowtopage(row_2)
 row_2.append(TAB)
 
 for (let i = 0; i < 12; i++) {
     mainPartKeyboard.push(document.createElement("div"));
-    mainPartKeyboard[i].classList.add("standart-key");
-    mainPartKeyboard[i].innerHTML = modifychars[i + 1];
-    row_2.append(mainPartKeyboard[i]);
+    Methods.addelemtopage(mainPartKeyboard[i], "standart-key", modifychars[i + 1], row_2)
 }
 
 const slashKey = document.createElement("div");
-slashKey.innerHTML = "/";
-row_2.append(slashKey);
-slashKey.classList.add("tab-slash");
+Methods.addelemtopage(slashKey, "tab-slash", "/", row_2)
 
-KEYBOARD.append(row_2);
+Methods.addrowtopage(row_2)
 
 const row_3 = document.createElement("div");
-row_3.classList.add("row");
+Methods.addrowtopage(row_3)
 
 
 const CAPSLOCK = document.createElement("div");
-CAPSLOCK.classList.add("caps-lock")
-CAPSLOCK.innerHTML = "CAPS<br>LOCK"
-row_3.append(CAPSLOCK);
+Methods.addelemtopage(CAPSLOCK, "caps-lock", "CAPS<br>LOCK", row_3)
+
+
 for (let i = 12; i < 23; i++) {
     mainPartKeyboard.push(document.createElement("div"));
-    mainPartKeyboard[i].classList.add("standart-key");
-    mainPartKeyboard[i].innerHTML = modifychars[i + 1];
-    row_3.append(mainPartKeyboard[i]);
+    Methods.addelemtopage(mainPartKeyboard[i], "standart-key", modifychars[i + 1], row_3)
 }
 
 const ENTER = document.createElement("div");
-ENTER.classList.add("enter")
-ENTER.innerHTML = "ENTER"
-row_3.append(ENTER);
-
-row_3.append()
-
-KEYBOARD.append(row_3);
-
+Methods.addelemtopage(ENTER, "enter", "ENTER", row_3)
 
 const row_4 = document.createElement("div");
-row_4.classList.add("row");
+Methods.addrowtopage(row_4)
+
 const SHIFT = document.createElement("div");
-SHIFT.classList.add("shift");
-SHIFT.innerHTML = "SHIFT";
-row_4.append(SHIFT);
+Methods.addelemtopage(SHIFT, "shift", "SHIFT", row_4)
+
 for (let i = 23; i < modifychars.length - 1; i++) {
     mainPartKeyboard.push(document.createElement("div"));
-    mainPartKeyboard[i].classList.add("standart-key");
-    mainPartKeyboard[i].innerHTML = modifychars[i + 1];
-    row_4.append(mainPartKeyboard[i]);
+    Methods.addelemtopage(mainPartKeyboard[i], "standart-key", modifychars[i + 1], row_4)
 }
 
-KEYBOARD.append(row_4);
 
+const arrowUp = document.createElement("div");
+Methods.addelemtopage(arrowUp, "standart-key", "↑", row_4)
+
+const SHIFT2 = document.createElement("div");
+Methods.addelemtopage(SHIFT2, "shift-two", "SHIFT", row_4)
+
+
+
+const row_5 = document.createElement("div");
+Methods.addrowtopage(row_5)
+
+const CTRL = document.createElement("div");
+Methods.addelemtopage(CTRL, "ctrl-alt-win", "CTRL", row_5)
+
+const ALT = document.createElement("div");
+Methods.addelemtopage(ALT, "ctrl-alt-win", "ALT", row_5);
+
+const SPACE = document.createElement("div");
+Methods.addelemtopage(SPACE, "space", "", row_5);
+
+const arrowLeft = document.createElement("div");
+Methods.addelemtopage(arrowLeft, "standart-key", "←", row_5);
+
+const arrowDown = document.createElement("div");
+Methods.addelemtopage(arrowDown, "standart-key", "↓", row_5);
+
+const arrowRight = document.createElement("div");
+Methods.addelemtopage(arrowRight, "standart-key", "→", row_5);
+
+const CTRL2 = document.createElement("div");
+Methods.addelemtopage(CTRL2, "ctrl-two", "CTRL", row_5);
+
+console.log(mainPartKeyboard)
 console.log(modifychars);
